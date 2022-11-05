cd6dc6accdb7ac4a074bbe6510ff3455687dc2f2
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-11-04 23:21:27 -0300

    Create EntityIMemoryCache.cs

---
+++ CacheTestes/CacheTestes/Caches/TesteIMemoryCache/EntityIMemoryCache.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace CacheTestes.Caches.TesteIMemoryCache
+{
+    public class EntityIMemoryCache
+    {
+        private readonly IMemoryCache _cache;
+
+        public EntityIMemoryCache(IMemoryCache cache)
+            => _cache = cache;
+
+        public void AddCache(List<Entity> ent)
+            => _cache.Set<List<Entity>>(key:"Entity-Cache", value: ent);
+    }
+}
