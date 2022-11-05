daad4bd1009d82998569fef5b4b8d91c996b2801
RafaelBonotto <68786414+RafaelBonotto@users.noreply.github.com>
2022-11-04 23:26:56 -0300

    Método para recuperar Cache

--- CacheTestes/CacheTestes/Caches/TesteIMemoryCache/EntityIMemoryCache.cs
+++ CacheTestes/CacheTestes/Caches/TesteIMemoryCache/EntityIMemoryCache.cs
@@ -5,11 +5,15 @@ namespace CacheTestes.Caches.TesteIMemoryCache
     public class EntityIMemoryCache
     {
         private readonly IMemoryCache _cache;
+        private const string KEY_ENTITY_CACHE = "Entity-Cache";
 
         public EntityIMemoryCache(IMemoryCache cache)
             => _cache = cache;
 
-        public void AddCache(List<Entity> ent)
-            => _cache.Set<List<Entity>>(key:"Entity-Cache", value: ent);
+        public void AddCache(Entity[] ent)
+            => _cache.Set<Entity[]>(key: KEY_ENTITY_CACHE, value: ent);
+
+        public Entity[] GetEntityCache()
+            => _cache.Get<Entity[]>(key: KEY_ENTITY_CACHE);
     }
 }
